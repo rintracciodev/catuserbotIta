6e82faff959b57ba61ac99044ceda1f35203dce4
Callato <87521685+Callato@users.noreply.github.com>
2021-08-20 16:25:34 +0200

    Add files via upload

---
+++ userbot/plugins/Developer.py
@@ -0,0 +1,16 @@
+A = (
+    "𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿 ====> @rintraccio ✅\n"
+    )
+
+
+@catub.cat_cmd(
+    pattern="Developer$",
+    command=("Developer", plugin_category),
+    info={
+        "header": "Per vedere chi è il Developer",
+        "usage": "{tr}Developer",
+    },
+)
+async def bluedevilDeveloper(Developer):
+    "fun art command"
+    await edit_or_reply(Developer, A)
