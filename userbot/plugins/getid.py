8989d49bcde82574172d8047cfd5080c53772914
Callato <87521685+Callato@users.noreply.github.com>
2021-08-21 16:55:04 +0200

    Update getid.py

--- userbot/plugins/getid.py
+++ userbot/plugins/getid.py
@@ -54,7 +54,7 @@ async def _(event):
         else:
             await edit_or_reply(
                 event,
-                f"**🆔: **`{r_msg.sender_id}`",
+                f"**</b> {username}\n" 🆔: **`{r_msg.sender_id}`",
             )
 
     else:
