16195cfa8389bf92faeda304e7003eef02192c46
Callato <87521685+Callato@users.noreply.github.com>
2021-08-29 19:27:04 +0200

    Update invite.py

--- userbot/plugins/invite.py
+++ userbot/plugins/invite.py
@@ -43,4 +43,4 @@ async def _(event):
             except Exception as e:
                 return await edit_delete(event, f"`{e}`", 5)
 
-    await edit_or_reply(event, f"`{to_add_users} is/are Invited Successfully`")
+    await edit_or_reply(event, f"`{to_add_users} aggiunto/i con successo✅`")
