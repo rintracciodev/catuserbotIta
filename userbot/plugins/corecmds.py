f300f418580cdbe0f46323acb9886c922320694a
rintracciodev <89912719+rintracciodev@users.noreply.github.com>
2021-10-11 16:52:55 +0200

    Update corecmds.py

--- userbot/plugins/corecmds.py
+++ userbot/plugins/corecmds.py
@@ -1,157 +1,131 @@
-import os
-from pathlib import Path
+# ==================================================================================================
+import asyncio
+import random
+from datetime import datetime
 
-from ..Config import Config
-from ..utils import load_module, remove_plugin
-from . import CMD_HELP, CMD_LIST, SUDO_LIST, catub, edit_delete, edit_or_reply, reply_id
+from userbot import catub
+
+from ..core.managers import edit_or_reply
+from ..sql_helper.globals import gvarstatus
+from . import catub, mention, reply_id
 
 plugin_category = "tools"
 
-DELETE_TIMEOUT = 5
-thumb_image_path = os.path.join(Config.TMP_DOWNLOAD_DIRECTORY, "thumb_image.jpg")
+# ===============================================
+normaltext = "1234567890."
+pingfont = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "0",
+]
+
+# ===============================================
 
 
 @catub.cat_cmd(
-    pattern="install$",
-    command=("install", plugin_category),
+    pattern="ping( -a|$)",
+    command=("ping", plugin_category),
     info={
-        "header": "To install an external plugin.",
-        "description": "Reply to any external plugin(supported by cat) to install it in your bot.",
-        "usage": "{tr}install",
+        "header": "check how long it takes to ping your userbot",
+        "flags": {"-a": "average ping"},
+        "option": "change ping design using `PING_TEMPLATE` (dv)",
+        "usage": ["{tr}ping", "{tr}ping -a"],
     },
 )
-async def install(event):
-    "To install an external plugin."
-    if event.reply_to_msg_id:
-        try:
-            downloaded_file_name = await event.client.download_media(
-                await event.get_reply_message(),
-                "userbot/plugins/",
-            )
-            if "(" not in downloaded_file_name:
-                path1 = Path(downloaded_file_name)
-                shortname = path1.stem
-                load_module(shortname.replace(".py", ""))
-                await edit_delete(
-                    event,
-                    f"Installed Plugin `{os.path.basename(downloaded_file_name)}`",
-                    10,
-                )
-            else:
-                os.remove(downloaded_file_name)
-                await edit_delete(
-                    event, "Errors! This plugin is already installed/pre-installed.", 10
-                )
-        except Exception as e:
-            await edit_delete(event, f"**Error:**\n`{e}`", 10)
-            os.remove(downloaded_file_name)
+async def _(event):
+    "To check ping"
+    flag = event.pattern_match.group(1)
+    start = datetime.now()
+    catevent = await edit_or_reply(event, f"`Caricamento..`")
+    end = datetime.now()
+    ms = str((end - start).microseconds / 1000)
+    for normal in ms:
+        if normal in normaltext:
+            pingchars = pingfont[normaltext.index(normal)]
+            ms = ms.replace(normal, pingchars)
+    my = f"Ecco il ping!!\n`{ms}` 𝔪𝔰\nCreatore: **{mention}**"
+    ping_caption = gvarstatus("PING_TEMPLATE") or my
 
-
-@catub.cat_cmd(
-    pattern="load ([\s\S]*)",
-    command=("load", plugin_category),
-    info={
-        "header": "To load a plugin again. if you have unloaded it",
-        "description": "To load a plugin again which you unloaded by {tr}unload",
-        "usage": "{tr}load <plugin name>",
-        "examples": "{tr}load markdown",
-    },
-)
-async def load(event):
-    "To load a plugin again. if you have unloaded it"
-    shortname = event.pattern_match.group(1)
-    try:
-        try:
-            remove_plugin(shortname)
-        except BaseException:
-            pass
-        load_module(shortname)
-        await edit_delete(event, f"`Successfully loaded {shortname}`", 10)
-    except Exception as e:
-        await edit_or_reply(
-            event,
-            f"Could not load {shortname} because of the following error.\n{e}",
+    if flag == " -a":
+        catevent = await edit_or_reply(event, "`!....`")
+        await asyncio.sleep(0.3)
+        await catevent.edit("`..!..`")
+        await asyncio.sleep(0.3)
+        await catevent.edit("`....!`")
+        end = datetime.now()
+        tms = (end - start).microseconds / 1000
+        ms = str(round((tms - 0.6) / 3, 3))
+        for normal in ms:
+            if normal in normaltext:
+                pingchars = pingfont[normaltext.index(normal)]
+                ms = ms.replace(normal, pingchars)
+        await catevent.edit(
+            f"<b>☞ Average Pong!<b>\n➥ <code>{ms} ms<code>",
+            parse_mode="html",
         )
-
-
-@catub.cat_cmd(
-    pattern="send ([\s\S]*)",
-    command=("send", plugin_category),
-    info={
-        "header": "To upload a plugin file to telegram chat",
-        "usage": "{tr}send <plugin name>",
-        "examples": "{tr}send markdown",
-    },
-)
-async def send(event):
-    "To uplaod a plugin file to telegram chat"
-    reply_to_id = await reply_id(event)
-    thumb = thumb_image_path if os.path.exists(thumb_image_path) else None
-    input_str = event.pattern_match.group(1)
-    the_plugin_file = f"./userbot/plugins/{input_str}.py"
-    if os.path.exists(the_plugin_file):
-        caat = await event.client.send_file(
-            event.chat_id,
-            the_plugin_file,
-            force_document=True,
-            allow_cache=False,
-            reply_to=reply_to_id,
-            thumb=thumb,
-            caption=f"**➥ Plugin Name:-** `{input_str}`",
-        )
-        await event.delete()
     else:
-        await edit_or_reply(event, "404: File Not Found")
+        ping_caption = gvarstatus("PING_TEMPLATE") or my
+        caption = ping_caption.format(ping=ms, mention=mention)
+        await catevent.edit(caption)
+
+
+# ===============================================
 
 
 @catub.cat_cmd(
-    pattern="unload ([\s\S]*)",
-    command=("unload", plugin_category),
+    pattern="mping$",
+    command=("mping", plugin_category),
     info={
-        "header": "To unload a plugin temporarily.",
-        "description": "You can load this unloaded plugin by restarting or using {tr}load cmd. Useful for cases like seting notes in rose bot({tr}unload markdown).",
-        "usage": "{tr}unload <plugin name>",
-        "examples": "{tr}unload markdown",
+        "header": "Checks the latency of userbot from the server, with a media",
+        "option": "change ping design using `PING_TEMPLATE` (dv)",
+        "usage": "{tr}mping",
     },
 )
-async def unload(event):
-    "To unload a plugin temporarily."
-    shortname = event.pattern_match.group(1)
-    try:
-        remove_plugin(shortname)
-        await edit_or_reply(event, f"Unloaded {shortname} successfully")
-    except Exception as e:
-        await edit_or_reply(event, f"Successfully unload {shortname}\n{e}")
-
-
-@catub.cat_cmd(
-    pattern="uninstall ([\s\S]*)",
-    command=("uninstall", plugin_category),
-    info={
-        "header": "To uninstall a plugin temporarily.",
-        "description": "To stop functioning of that plugin and remove that plugin from bot.",
-        "note": "To unload a plugin permanently from bot set NO_LOAD var in heroku with that plugin name, give space between plugin names if more than 1.",
-        "usage": "{tr}uninstall <plugin name>",
-        "examples": "{tr}uninstall markdown",
-    },
-)
-async def unload(event):
-    "To uninstall a plugin."
-    shortname = event.pattern_match.group(1)
-    path = Path(f"userbot/plugins/{shortname}.py")
-    if not os.path.exists(path):
-        return await edit_delete(
-            event, f"There is no plugin with path {path} to uninstall it"
+async def _(event):
+    "Shows ping with a given random media"
+    if event.fwd_from:
+        return
+    reply_to_id = await reply_id(event)
+    # add space b/w each telegraph link
+    PING_PICS = (
+        gvarstatus("PING_PICS")
+        or "https://telegra.ph/file/1328d62db93ad22b69ba2.jpg https://telegra.ph/file/b2da6e4c55dd29600e4ed.jpg"
+    )
+    PING_PICS = PING_PICS.rsplit(" ")
+    start = datetime.now()
+    await edit_or_reply(event, f"`Checking`")
+    end = datetime.now()
+    ms = str((end - start).microseconds / 1000)
+    for normal in ms:
+        if normal in normaltext:
+            pingchars = pingfont[normaltext.index(normal)]
+            ms = ms.replace(normal, pingchars)
+    my = f"Ecco il ping!!\n`{ms}` 𝔪𝔰\nCreatore: **{mention}**"
+    ping_caption = gvarstatus("PING_TEMPLATE") or my
+    caption = ping_caption.format(ping=ms, mention=mention)
+    PING_PIC = random.choice(PING_PICS)
+    if PING_PIC:
+        while PING_PIC == "":
+            try:
+                PING_PIC = random.choice(PING_PICS)
+            except IndexError:
+                pass
+        await event.delete()
+        await event.client.send_file(
+            event.chat_id,
+            PING_PIC,
+            caption=caption,
+            reply_to=reply_to_id,
+            link_preview=False,
+            allow_cache=True,
         )
-    os.remove(path)
-    if shortname in CMD_LIST:
-        CMD_LIST.pop(shortname)
-    if shortname in SUDO_LIST:
-        SUDO_LIST.pop(shortname)
-    if shortname in CMD_HELP:
-        CMD_HELP.pop(shortname)
-    try:
-        remove_plugin(shortname)
-        await edit_or_reply(event, f"{shortname} is Uninstalled successfully")
-    except Exception as e:
-        await edit_or_reply(event, f"Successfully uninstalled {shortname}\n{e}")
+
+
+# ==================================================================================================
